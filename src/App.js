dc33ae87decf569e15c88eda09b419da47f20f3e
sam gmarvis <sgmarvis@gmail>
2024-03-19 08:45:01 +0100

    firsts commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from "react-redux";
+import "./App.css";
 
 function App() {
+  const state = useSelector((state) => state);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>welcome to react with Redux</h1>
     </div>
   );
 }
