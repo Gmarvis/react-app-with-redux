0d6a93b2ba245e5bc10380517365a3c17667cdfb
sam gmarvis <sgmarvis@gmail>
2024-03-22 06:36:22 +0100

    added action creators

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state/index";
 import "./App.css";
 
 function App() {
-  const state = useSelector((state) => state);
+  const account = useSelector((state) => state.account);
+
+  const dispatch = useDispatch();
+
+  const { depsitMoney, withdrawMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+  console.log(account);
   return (
     <div className="App">
-      <h1>welcome to react with Redux</h1>
+      <h1>{account}</h1>
+      <button onClick={() => depsitMoney(1000)}>depisit</button>
+      <button onClick={() => withdrawMoney(1000)}>withdraw</button>
     </div>
   );
 }
